5143cf69cc76e4da76ee4ade667d772c0d431c1b
David Santia <davidsantia.morris@gmail.com>
2019-11-04 13:05:53 -0700

    Add routines to use MSSQL instead of Redis

--- src/main/java/com/newrelic/servlet/SqlConnect.java
+++ src/main/java/com/newrelic/servlet/SqlConnect.java
@@ -8,31 +8,31 @@ import java.sql.SQLException;
  * @author davidmorris
  */
 public class SqlConnect {
-    private Connection connection;
+	private Connection connection;
 
-    public SqlConnect (String host) {
-        String connectionUrl = "jdbc:sqlserver:/" + host +
-        		":1433;databaseName=words;user=demo;password=Welcome:123";
-        try {
-            // connect to SQL server
-        	this.connection = DriverManager.getConnection(connectionUrl);
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        System.out.println("Connected to SQL server");
-    }
+	public SqlConnect(String host) {
+		String connectionUrl = "jdbc:sqlserver://" + host + ":1433;databaseName=words;user=demo;password=Welcome:123";
+		try {
+			// connect to SQL server
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			this.connection = DriverManager.getConnection(connectionUrl);
+		} catch (SQLException | ClassNotFoundException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Connected to SQL server");
+	}
 
-    public Connection conn() {
-        return this.connection;
-    }
+	public Connection conn() {
+		return this.connection;
+	}
 
-    public void close() {
-        try {
-            this.connection.close();
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
+	public void close() {
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+	}
 }
