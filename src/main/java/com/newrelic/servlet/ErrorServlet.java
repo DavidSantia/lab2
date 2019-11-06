a2df4b312594f6508417db5b79ad437fb5a6ae7f
David Santia <davidsantia.morris@gmail.com>
2019-11-06 13:33:54 -0700

    format code

--- src/main/java/com/newrelic/servlet/ErrorServlet.java
+++ src/main/java/com/newrelic/servlet/ErrorServlet.java
@@ -20,32 +20,33 @@ import org.apache.logging.log4j.Logger;
 public class ErrorServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-    private static final Logger logger = LogManager.getLogger(ErrorServlet.class);
+	private static final Logger logger = LogManager.getLogger(ErrorServlet.class);
 
-    // Game day scenario - Java heap out of memory
-    protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-        int iteratorValue = 20;
-        for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
-            logger.info("FillMemory iteration #" + outerIterator + ", " + Runtime.getRuntime().freeMemory() + " bytes free");
-            int loop1 = 2;
-            int[] memoryFillIntVar = new int[iteratorValue];
-            do {
-                memoryFillIntVar[loop1] = 0;
-                loop1--;
-            } while (loop1 > 0);
-            iteratorValue = iteratorValue * 5;
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException e) {
-               logger.warn("Thread is interrupted - " + e);
-            }
-        }
-        
-        // return HTML response
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>\n<h1>Using up lots of memory</h1>\n</html>");
-        
-    }
+	// Game day scenario - Java heap out of memory
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int iteratorValue = 20;
+		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+			logger.info("FillMemory iteration #" + outerIterator + ", " + Runtime.getRuntime().freeMemory()
+					+ " bytes free");
+			int loop1 = 2;
+			int[] memoryFillIntVar = new int[iteratorValue];
+			do {
+				memoryFillIntVar[loop1] = 0;
+				loop1--;
+			} while (loop1 > 0);
+			iteratorValue = iteratorValue * 5;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				logger.warn("Thread is interrupted - " + e);
+			}
+		}
+
+		// return HTML response
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>\n<h1>Using up lots of memory</h1>\n</html>");
+
+	}
 
 }
