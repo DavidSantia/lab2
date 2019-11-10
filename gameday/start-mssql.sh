f5b00c56a873113412f47662fb590d44407b1c4c
David Santia <davidsantia.morris@gmail.com>
2019-11-10 15:23:16 -0700

    Fix default port on mssql

--- gameday/start-mssql.sh
+++ gameday/start-mssql.sh
@@ -1,2 +1,2 @@
 # MSSQL for Java app
-docker run -d --rm --name mssql -p 1443:1443 --log-driver=fluentd --log-opt tag="nrlogs" mssql
+docker run -d --rm --name mssql -p 1433:1433 --log-driver=fluentd --log-opt tag="nrlogs" mssql
