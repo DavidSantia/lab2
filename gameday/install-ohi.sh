61bb226223bbd8d7559fba7408746d1a187081e0
David Santia <davidsantia.morris@gmail.com>
2019-12-11 10:36:50 -0700

    Remove golang image after MSSQL OHI build

--- gameday/install-ohi.sh
+++ gameday/install-ohi.sh
@@ -8,6 +8,7 @@ docker pull golang
 docker run --rm -v $ROOT/lab2/nri-mssql:/go/src/github.com/newrelic/nri-mssql --entrypoint sh golang \
   -c "cd /go/src/github.com/newrelic/nri-mssql; make"
 cd $ROOT/lab2/nri-mssql
+docker rmi golang
 sudo cp -r bin /var/db/newrelic-infra/newrelic-integrations/
 sudo cp mssql-definition.yml /var/db/newrelic-infra/newrelic-integrations/
 sudo cp mssql-config.yml.sample /etc/newrelic-infra/integrations.d/mssql-config.yml
